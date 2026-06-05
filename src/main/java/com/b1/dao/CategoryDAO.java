5f4fd15a78a7a83520097c15a011899ba9542c0d
harshanand2021 <harsh25061999@gmail.com>
2026-06-05 13:17:05 +0530

    Interface modified

---
+++ src/main/java/com/b1/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.b1.dao;
+
+import com.b1.entity.Category;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+
+public interface CategoryDAO {
+
+    public Iterator<Category> getAllCategories() throws SQLException;
+}
