d85625d97a79f843e76036b4f9a2c237588c98ec
Harsh Anand <harsh25061999@gmail.com>
2026-06-08 00:28:03 +0530

    Update on 8-6-26 12:27 AM

--- src/main/java/com/b1/entity/Category.java
+++ src/main/java/com/b1/entity/Category.java
@@ -1,55 +1,21 @@
 package com.b1.entity;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+public class Category {
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.*;
+    int categoryId;
+    String categoryName;
 
-@WebServlet("/Category")
-public class Category extends HttpServlet{
-    private static final long serialVersionUID = 1L;
+    public Category() {}
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException {
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_project","root","cdac");
-                PreparedStatement psAllCategories = connection.prepareStatement("Select * From Category")){
-                try(ResultSet result = psAllCategories.executeQuery()){
-                    PrintWriter out = response.getWriter();
-                    out.println("<html>");
-                    out.println("<body>");
-                    out.println("<table border='1'>");
-                    out.println("<tr>");
-                    out.println("<th>Name</th>");
-                    out.println("<th>Description</th>");
-                    out.println("<th>Image</th>");
-                    out.println("</tr>");
+    public Category(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
 
-                    while(result.next()){
-                        out.println("<tr>");
-                        out.println("<td><a href='Products?CategoryId=" + result.getInt("categoryId")
-                        + "'>" + result.getString("categoryName") + "</a></td>" + "<td>"+
-                                result.getString("categoryDescription") + "</td>"
-                                );
-                        out.println("<td><img src=Images/" + result.getString("categoryImageUrl") + "' height='80px' width='80px'/></td>");
-                        out.println("</tr>");
-                    }
-                    out.println("</table>");
-                    out.println("</body>");
-                    out.println("</html>");
-                }catch(SQLException e){
-                    e.printStackTrace();
-                }
-            }  catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }catch(ClassNotFoundException e){
-            e.printStackTrace();
-        }
+    public int getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 }
--- src/main/java/com/b1/entity/Users.java
+++ src/main/java/com/b1/entity/Users.java
@@ -1,4 +1,51 @@
 package com.b1.entity;
 
 public class Users  {
+
+    int userId;
+    String userName;
+    String userEmail;
+    String userPassword;
+
+    public Users(){}
+
+    public Users(String userName,String userEmail,String userPassword){
+        super();
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
 }
